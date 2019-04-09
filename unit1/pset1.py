cc4a78e4ccb34dfc27e1f09139367736ab307a1a
Gilmar Neves <gilmarneves@outlook.com>
2019-04-09 00:47:30 -0300

    Update pset1.py

--- unit1/pset1.py
+++ unit1/pset1.py
@@ -1,1 +1,6 @@
-#
+'''
+A colony of Aucks (super-intelligent alien bioengineers) has landed on Earth and has created new species of farm animals! 
+The Aucks are performing their experiments on Earth, and plan on transporting the mutant animals back to their home planet of Aurock. 
+In this problem set, you will implement algorithms to figure out how the aliens should shuttle their experimental animals back across space.
+'''
+
